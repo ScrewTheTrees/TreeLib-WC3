05d78fdd0c3c160edfca0e59216cb2a584f96ee5
ScrewTheTrees <fredrik@grimmenhag.se>
2020-06-30 20:26:42 +0200

    Renaming

--- src/TreeLib/Utility/Extended Functionality/UnitArrayFunc.ts
+++ src/TreeLib/Utility/Extended Functionality/UnitArrayFunc.ts
@@ -1,25 +1,27 @@
-const _enumGroupToUse = CreateGroup();
+export namespace UnitArrays {
+    const _enumGroupToUse = CreateGroup();
 
-export function ArrayUnitsInRange(x: number, y: number, radius: number, filter?: (filterUnit: unit) => boolean) {
-    let array: unit[] = [];
-    let filterEnum = Filter(() => {
-        if (filter == null || filter(GetFilterUnit()))
-            array.push(GetFilterUnit());
-        return false;
-    });
-    GroupEnumUnitsInRange(_enumGroupToUse, x, y, radius, filterEnum);
-    DestroyFilter(filterEnum);
-    return array;
-}
+    export function ArrayUnitsInRange(x: number, y: number, radius: number, filter?: (filterUnit: unit) => boolean) {
+        let array: unit[] = [];
+        let filterEnum = Filter(() => {
+            if (filter == null || filter(GetFilterUnit()))
+                array.push(GetFilterUnit());
+            return false;
+        });
+        GroupEnumUnitsInRange(_enumGroupToUse, x, y, radius, filterEnum);
+        DestroyFilter(filterEnum);
+        return array;
+    }
 
-export function ArrayUnitsOfPlayer(p: player, filter?: (filterUnit: unit) => boolean) {
-    let array: unit[] = [];
-    let filterEnum = Filter(() => {
-        if (filter == null || filter(GetFilterUnit()))
-            array.push(GetFilterUnit());
-        return false;
-    });
-    GroupEnumUnitsOfPlayer(_enumGroupToUse, p, filterEnum);
-    DestroyFilter(filterEnum);
-    return array;
+    export function ArrayUnitsOfPlayer(p: player, filter?: (filterUnit: unit) => boolean) {
+        let array: unit[] = [];
+        let filterEnum = Filter(() => {
+            if (filter == null || filter(GetFilterUnit()))
+                array.push(GetFilterUnit());
+            return false;
+        });
+        GroupEnumUnitsOfPlayer(_enumGroupToUse, p, filterEnum);
+        DestroyFilter(filterEnum);
+        return array;
+    }
 }
