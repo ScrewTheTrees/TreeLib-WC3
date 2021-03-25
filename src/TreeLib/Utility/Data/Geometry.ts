771b7bb9700ca07876d0be9d609fcd6fa2e74deb
ScrewTheTrees <fredrik@grimmenhag.se>
2021-03-25 20:29:31 +0100

    Remove geometry.

--- src/TreeLib/Utility/Data/Geometry.ts
+++
@@ -1,6 +0,0 @@
-import {Rectangle} from "./Rectangle";
-import {Vector2} from "./Vector2";
-import {Vector3} from "./Vector3";
-import {Cube} from "./Cube";
-
-export type Geometry = Vector2 | Vector3 | Rectangle | Cube;
