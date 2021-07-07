7d64af1dcfa564ac93eea5d7ca7eac509093f881
ScrewTheTrees <fredrik@grimmenhag.se>
2021-07-07 21:22:45 +0200

    Changed location of interpolation.

--- src/TreeLib/Misc.ts
+++ src/TreeLib/Misc.ts
@@ -2,46 +2,6 @@
  * This is just very Miscellaneous functions used for mostly movement and rotation.
  */
 
-export function linearInterpolate(start: number, end: number, maxStep: number = 1) {
-    let diff = start - end;
-    if (diff < 0) {
-        return start + (math.max(maxStep, diff));
-    } else {
-        return start + (math.min(-maxStep, diff));
-    }
-}
-
-export function divisionInterpolate(start: number, end: number, divider: number, minimumStep: number = 1) {
-    let diff = start - end;
-    if (diff <= minimumStep && diff >= -minimumStep) {
-        return diff;
-    }
-
-    return start + (-diff / divider);
-}
-
-
-export function rotateToPoint(fromDir: number, toDir: number, turnSpeed: number) {
-    let result = toDir - fromDir;
-    while (result > 180) {
-        result -= 360
-    }
-    while (result < -180) {
-        result += 360
-    }
-    let turnDir = result;
-
-    if (turnDir < -turnSpeed) {
-        turnDir = -turnSpeed
-    }
-    if (turnDir > turnSpeed) {
-        turnDir = turnSpeed
-    }
-
-    return (fromDir + turnDir) % 360;
-}
-
-
 export function ChooseOne<T>(...input: T[]): T {
     let random = GetRandomInt(0, input.length - 1);
     return input[random];
--- src/TreeLib/Utility/Interpolation.ts
+++ src/TreeLib/Utility/Interpolation.ts
@@ -12,4 +12,42 @@ export namespace Interpolation {
         let f = (1 - Math.cos(ft)) * 0.5;
         return a * (1 - f) + b * f;
     }
+
+    export function Lerp(start: number, end: number, maxStep: number = 1) {
+        let diff = start - end;
+        if (diff < 0) {
+            return start + (math.max(maxStep, diff));
+        } else {
+            return start + (math.min(-maxStep, diff));
+        }
+    }
+
+    export function DivisionSpring(start: number, end: number, divider: number, minimumStep: number = 1) {
+        let diff = start - end;
+        if (diff <= minimumStep && diff >= -minimumStep) {
+            return diff;
+        }
+
+        return start + (-diff / divider);
+    }
+
+    export function RotateToPoint(fromDir: number, toDir: number, turnSpeed: number) {
+        let result = toDir - fromDir;
+        while (result > 180) {
+            result -= 360
+        }
+        while (result < -180) {
+            result += 360
+        }
+        let turnDir = result;
+
+        if (turnDir < -turnSpeed) {
+            turnDir = -turnSpeed
+        }
+        if (turnDir > turnSpeed) {
+            turnDir = turnSpeed
+        }
+
+        return (fromDir + turnDir) % 360;
+    }
 }
